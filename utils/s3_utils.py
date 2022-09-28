5e303a5bd9cfb0c7748eb22cbb164f9d6f3403ba
JacopoMalatesta <jacopomalatesta95@gmail.com>
2022-09-28 15:22:35 +0200

    Added missing docstrings to s3_utils.py

--- utils/s3_utils.py
+++ utils/s3_utils.py
@@ -11,15 +11,18 @@ info_log = Logger(name=__name__, level=logging.INFO).return_logger()
 
 
 def get_boto_session(config: dict) -> boto3.session.Session:
+    """Returns a Boto3 session"""
     return boto3.Session(aws_access_key_id=config["aws_access_key"],
                          aws_secret_access_key=config["aws_secret_access_key"])
 
 
 def get_s3_client(session: boto3.session.Session) -> boto3.session.Session.client:
+    """Returns an S3 client"""
     return session.client('s3')
 
 
 def get_s3_resource(session: boto3.session.Session) -> boto3.session.Session.resource:
+    """Returns an s3 resource"""
     return session.resource('s3')
 
 
@@ -32,6 +35,7 @@ def get_s3_key(metadata: dict) -> str:
 def read_csv_from_s3(bucket: str,
                      s3_client: boto3.session.Session.client,
                      key: str) -> pd.DataFrame or None:
+    """Reads the .csv file containing the playlist from S3"""
     try:
         obj = s3_client.get_object(Bucket=bucket, Key=key)
         buffer = io.BytesIO(obj['Body'].read())
@@ -46,6 +50,7 @@ def write_csv_to_s3(bucket: str,
                     s3_resource: boto3.session.Session.resource,
                     filename: str,
                     df: pd.DataFrame):
+    """Writes a Pandas dataframe to S3 as a .csv file"""
     csv_buffer = io.BytesIO()
     df.to_csv(csv_buffer, index=False)
     s3_resource.Object(bucket, f'{filename}').put(Body=csv_buffer.getvalue())
